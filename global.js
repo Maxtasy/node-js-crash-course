3582736c1c867bb590994046d59a196aa395028b
Maxtasy <4960131+Maxtasy@users.noreply.github.com>
2021-04-21 21:05:48 +0200

    Finish lesson

---
+++ global.js
@@ -0,0 +1,15 @@
+// Global object
+
+// console.log(global)
+
+// setTimeout(() => {
+//   console.log("timeout over")
+//   clearInterval(int)
+// }, 3000)
+
+// const int = setInterval(() => {
+//   console.log("interval")
+// }, 1000)
+
+// console.log(__dirname)
+// console.log(__filename)
---
+++ people.js
@@ -0,0 +1,9 @@
+const people = ["Max", "Dave", "Andrej", "Pascal"]
+const ages = [20, 25, 30, 35]
+
+// console.log(people)
+
+module.exports = {
+  people,
+  ages,
+}
