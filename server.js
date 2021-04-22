429bb31cb5d6b65889b3a6b6c4da1f94e606d007
Maxtasy <4960131+Maxtasy@users.noreply.github.com>
2021-04-22 20:53:19 +0200

    Finish lesson

--- server.js
+++ server.js
@@ -1,7 +1,50 @@
 const http = require("http")
+const fs = require("fs")
 
 const server = http.createServer((req, res) => {
-  console.log("Request made")
+  console.log(req.url, req.method)
+
+  // Set header content type
+  res.setHeader("Content-Type", "text/html")
+
+  // Send raw text/html
+  // res.write("<head><link rel='stylesheet' href='#'></head>")
+  // res.write("<p>hello world</p>")
+  // res.write("<p>hello again world</p>")
+  // res.end()
+
+  let path = "./views/"
+
+  switch (req.url) {
+    case "/":
+      path += "index.html"
+      res.statusCode = 200
+      break
+    case "/about":
+      path += "about.html"
+      res.statusCode = 200
+      break
+    case "/about-me":
+      res.statusCode = 301
+      res.setHeader("Location", "/about")
+      res.end()
+      break
+    default:
+      path += "404.html"
+      res.statusCode = 404
+      break
+  }
+
+  // Send an html file
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err)
+      res.end()
+    } else {
+      // res.write(data)
+      res.end(data)
+    }
+  })
 })
 
 server.listen(3000, "localhost", () => {
