de6993595288d678cc21a6c3e32c0fb85c47e664
Maxtasy <4960131+Maxtasy@users.noreply.github.com>
2021-06-19 20:25:47 +0200

    Finish lesson

--- app.js
+++ app.js
@@ -24,6 +24,9 @@ mongoose
 // set public directory from which we are serving the static files
 app.use(express.static(__dirname + "/public"));
 
+// needed so the form data gets send as an object with the post request
+app.use(express.urlencoded({ extended: true }));
+
 // register view engine
 app.set("view engine", "ejs");
 
@@ -44,6 +47,52 @@ app.get("/blogs", (req, res) => {
     .sort({ createdAt: -1 })
     .then((result) => {
       res.render("index", { title: "All Blogs", blogs: result });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.post("/blogs", (req, res) => {
+  const { title, snippet, body } = req.body;
+
+  const blog = new Blog({
+    title,
+    snippet,
+    body,
+  });
+
+  // Or shorter
+  // const blog = new Blog({ req.body });
+
+  blog
+    .save()
+    .then((result) => {
+      res.redirect("/blogs");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get("/blogs/:id", (req, res) => {
+  const id = req.params.id;
+
+  Blog.findById(id)
+    .then((result) => {
+      res.render("details", { title: "Blog Details", blog: result });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.delete("/blogs/:id", (req, res) => {
+  const id = req.params.id;
+
+  Blog.findByIdAndDelete(id)
+    .then((result) => {
+      res.json({ redirect: "/blogs" });
     })
     .catch((err) => {
       console.log(err);
