5a0498b7860886fa3b0d2805d3e18d6fd1b70026
Maxtasy <4960131+Maxtasy@users.noreply.github.com>
2021-06-19 18:12:34 +0200

    Finish lesson

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
+const Blog = require("./models/blog");
 
 // express app
 const app = express();
+
+// connect to mongodb
+const dbURI =
+  "mongodb+srv://maxtasy:test12345@cluster0.j1g6a.mongodb.net/nodejs-crash-course?retryWrites=true&w=majority";
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => {
+    // console.log("connected to database");
+
+    // listen for requests
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 // set public directory from which we are serving the static files
 app.use(express.static(__dirname + "/public"));
@@ -10,33 +27,27 @@ app.use(express.static(__dirname + "/public"));
 // register view engine
 app.set("view engine", "ejs");
 
-// listen for requests
-app.listen(3000);
-
 // package to log requests
 app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
-  const blogs = [
-    {
-      title: "Yoshi finds egg",
-      snippet: "Lorem ipsum dolor sit amet consectetur",
-    },
-    {
-      title: "Mario finds stars",
-      snippet: "Lorem ipsum dolor sit amet consectetur",
-    },
-    {
-      title: "How to defeat bowser",
-      snippet: "Lorem ipsum dolor sit amet consectetur",
-    },
-  ];
-
-  res.render("index", { title: "Home", blogs });
+  res.redirect("/blogs");
 });
 
 app.get("/about", (req, res) => {
   res.render("about", { title: "About" });
+});
+
+// blog routes
+app.get("/blogs", (req, res) => {
+  Blog.find()
+    .sort({ createdAt: -1 })
+    .then((result) => {
+      res.render("index", { title: "All Blogs", blogs: result });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 app.get("/blogs/create", (req, res) => {
