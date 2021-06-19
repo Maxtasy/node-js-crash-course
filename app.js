5cf3ffd117737d36bf1640e01e472474cd4ea82c
Maxtasy <4960131+Maxtasy@users.noreply.github.com>
2021-06-19 17:22:52 +0200

    Finish lesson

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const morgan = require("morgan");
 
 // express app
 const app = express();
@@ -11,6 +12,9 @@ app.set("view engine", "ejs");
 
 // listen for requests
 app.listen(3000);
+
+// package to log requests
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   const blogs = [
